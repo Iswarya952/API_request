a6f57e340cdeb8b9bf4e7f3e4737ead1fddc3574
Iswarya952 <24b01a1232@svecw.edu.in>
2025-09-29 19:32:23 +0530

    request get file add

---
+++ api_request.py
@@ -0,0 +1,17 @@
+import requests
+#GET REQUEST
+response=requests.get("https://jsonplaceholder.typicode.com/posts")
+print(response)
+print(response.status_code)
+print(response.json())
+#Filtering the data
+params={"userId":2}
+response=requests.get("https://jsonplaceholder.typicode.com/posts",params)
+print(response)
+print(response.status_code)
+print(response.json())
+#POST
+data={"title":"Hello","body":"World","userId":101}
+response=requests.post("https://jsonplaceholder.typicode.com/posts",json=data)
+print(response.status_code)
+print(response.json())
