9378f81a9f18e9d2c77d0325b4dd8fd47b94acd1
Iswarya952 <24b01a1232@svecw.edu.in>
2025-09-30 08:32:51 +0530

    filtering the data content is added

--- api_request.py
+++ api_request.py
@@ -7,11 +7,4 @@ print(response.json())
 #Filtering the data
 params={"userId":2}
 response=requests.get("https://jsonplaceholder.typicode.com/posts",params)
-print(response)
-print(response.status_code)
-print(response.json())
-#POST
-data={"title":"Hello","body":"World","userId":101}
-response=requests.post("https://jsonplaceholder.typicode.com/posts",json=data)
-print(response.status_code)
 print(response.json())
