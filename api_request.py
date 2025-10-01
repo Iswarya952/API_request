0fba7563d93387028d2603fc53bad9384b1152c5
Iswarya952 <24b01a1232@svecw.edu.in>
2025-10-01 17:48:58 +0530

    post file add to api request

--- api_request.py
+++ api_request.py
@@ -8,3 +8,8 @@ print(response.json())
 params={"userId":2}
 response=requests.get("https://jsonplaceholder.typicode.com/posts",params)
 print(response.json())
+#POST
+data={"title": "Hello","body":"World","userId":101}
+response=requests.post("https://jsonplaceholder.typicode.com/posts",json=data)
+print(response.status_code)
+print(response.json())
